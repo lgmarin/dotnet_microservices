c3ae0411df7921efdfa38de38cf9a213e17d1867
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-07-05 15:50:00 -0300

    Create ProductRepository

---
+++ WebShop.ProductApi/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+using WebShop.ProductApi.Models;
+
+namespace WebShop.ProductApi.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAll();
+    Task<Product> GetById(int id);
+
+    Task<Product> Create(Product product);
+    Task<Product> Update(Product product);
+    Task<Product> Delete(int id);
+}
