0b3ab5339a7445face729f5dddb06aea19ed4762
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-07-06 11:04:47 -0300

    Create Products Controller

---
+++ WebShop.ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore.Mvc;
+using WebShop.ProductApi.DTOs;
+using WebShop.ProductApi.Services;
+
+namespace WebShop.ProductApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public ProductsController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+    {
+        var categoriesDto = await _categoryService.GetCategories();
+        if (categoriesDto is null)
+        {
+            return NotFound("No categories found.");
+        }
+        return Ok(categoriesDto);
+    }
+    
+    [HttpGet("products")]
+    public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetcategoriesProducts()
+    {
+        var categoriesDto = await _categoryService.GetCategoriesProducts();
+        if (categoriesDto is null)
+        {
+            return NotFound("No categories found.");
+        }
+        return Ok(categoriesDto);
+    }
+
+    [HttpGet("{id:int}", Name = "GetCategory")]
+    public async Task<ActionResult<CategoryDTO>> Get(int id)
+    {
+        var categoryDto = await _categoryService.GetCategoryById(id);
+        if (categoryDto is null)
+        {
+            return NotFound("Category not found");
+        }
+
+        return Ok(categoryDto);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDto)
+    {
+        if (categoryDto is null)
+        {
+            return BadRequest("Invalid data.");
+        }
+
+        await _categoryService.AddCategory(categoryDto);
+
+        return new CreatedAtRouteResult("GetCategory", new { id = categoryDto.CategoryId }, categoryDto);
+    }
+
+    [HttpPut("{id:int")]
+    public async Task<ActionResult> Put(int id, [FromBody] CategoryDTO categoryDto)
+    {
+        if (id != categoryDto.CategoryId)
+            return BadRequest();
+
+        if (categoryDto is null)
+            return BadRequest();
+
+        await _categoryService.UpdateCategory(categoryDto);
+
+        return Ok(categoryDto);
+    }
+}
+
