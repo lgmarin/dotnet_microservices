95a177c2197723e8eae0f0a1ccdc4c7bccbc7d0c
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-07-06 09:18:50 -0300

    Create ProductService.cs

--- WebShop.ProductApi/Services/ProductService.cs
+++ WebShop.ProductApi/Services/ProductService.cs
@@ -1,8 +1,49 @@
+using AutoMapper;
+using WebShop.ProductApi.DTOs;
+using WebShop.ProductApi.Models;
+using WebShop.ProductApi.Repositories;
+
 namespace WebShop.ProductApi.Services;
 
-public class ProductServiceervice : IProductService
+public class ProductService : IProductService
 {
-    public ProductServiceervice()
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IProductRepository productRepository, IMapper mapper)
     {
+        _productRepository = productRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<ProductDTO>> GetProducts()
+    {
+        var productEntity = await _productRepository.GetAll();
+        return _mapper.Map<IEnumerable<ProductDTO>>(productEntity);
+    }
+
+    public async Task<ProductDTO> GetProductById(int id)
+    {
+        var productEntity = await _productRepository.GetById(id);
+        return _mapper.Map<ProductDTO>(productEntity);
+    }
+
+    public async Task AddProduct(ProductDTO productDto)
+    {
+        var productEntity = _mapper.Map<Product>(productDto);
+        await _productRepository.Create(productEntity);
+        productDto.Id = productEntity.Id;
+    }
+
+    public async Task UpdateProduct(ProductDTO productDto)
+    {
+        var productEntity = _mapper.Map<Product>(productDto);
+        await _productRepository.Update(productEntity);
+    }
+
+    public async Task RemoveProduct(int id)
+    {
+        var productEntity = _productRepository.GetById(id).Result;
+        await _productRepository.Delete(productEntity.Id);
     }
 }
