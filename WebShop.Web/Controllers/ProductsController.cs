346fecfda8c6f7b6f3aaf8fa8d9e8fd2c87a1c50
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-07-07 11:51:19 -0300

    Create Product Index View and associated Controllers

--- WebShop.Web/Controllers/ProductsController.cs
+++ WebShop.Web/Controllers/ProductsController.cs
@@ -1,13 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Web.Models;
 using WebShop.Web.Services.Contracts;
 
 namespace WebShop.Web.Controllers;
 
-public class ProductsController
+public class ProductsController : Controller
 {
     private readonly IProductService _productService;
 
     public ProductsController(IProductService productService)
     {
         _productService = productService;
+    }
+
+    public async Task<ActionResult<IEnumerable<ProductViewModel>>> Index()
+    {
+        var result = await _productService.GetAllProducts();
+
+        if (result is null)
+        {
+            return View("Error");
+        }
+        return View(result);
+    }
+
+    public IActionResult CreateProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IActionResult UpdateProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IActionResult DeleteProduct()
+    {
+        throw new NotImplementedException();
     }
 }
