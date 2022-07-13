bbd9044e14ff2c1bd7ae9518c2e4a29194754afc
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-07-13 10:48:12 -0300

    Fix Bugs

--- WebShop.IdentityServer/Services/ProfileAppService.cs
+++ WebShop.IdentityServer/Services/ProfileAppService.cs
@@ -23,49 +23,49 @@ public class ProfileAppService : IProfileService
 
     public async Task GetProfileDataAsync(ProfileDataRequestContext context)
     {
-//id do usuário no IS
-            string id = context.Subject.GetSubjectId();
-            //localiza o usuário pelo id
-            ApplicationUser user = await _userManager.FindByIdAsync(id);
-            
-            //cria ClaimsPrincipal para o usuario
-            ClaimsPrincipal userClaims = await _userClaimsPrincipalFactory
-                                               .CreateAsync(user);
-            //define uma coleção de claims para o usuário
-            //e inclui o sobrenome e o nome do usuário
-            List<Claim> claims = userClaims.Claims.ToList();
-            claims.Add(new Claim(JwtClaimTypes.FamilyName, user.LastName));
-            claims.Add(new Claim(JwtClaimTypes.GivenName, user.FirstName));
+        //id do usuário no IS
+        string id = context.Subject.GetSubjectId();
+        //localiza o usuário pelo id
+        ApplicationUser user = await _userManager.FindByIdAsync(id);
 
-            //se o userManager do identity suportar role
-            if (_userManager.SupportsUserRole)
+        //cria ClaimsPrincipal para o usuario
+        ClaimsPrincipal userClaims = await _userClaimsPrincipalFactory
+                                           .CreateAsync(user);
+        //define uma coleção de claims para o usuário
+        //e inclui o sobrenome e o nome do usuário
+        List<Claim> claims = userClaims.Claims.ToList();
+        claims.Add(new Claim(JwtClaimTypes.FamilyName, user.LastName));
+        claims.Add(new Claim(JwtClaimTypes.GivenName, user.FirstName));
+
+        //se o userManager do identity suportar role
+        if (_userManager.SupportsUserRole)
+        {
+            //obtem a lista dos nomes das roles para o usuário
+            IList<string> roles = await _userManager.GetRolesAsync(user);
+            //percorre a lista
+            foreach (string role in roles)
             {
-                //obtem a lista dos nomes das roles para o usuário
-                IList<string> roles = await _userManager.GetRolesAsync(user);
-                //percorre a lista
-                foreach (string role in roles)
-                {
-                    //adiciona a role na claim
-                    claims.Add(new Claim(JwtClaimTypes.Role, role));
+                //adiciona a role na claim
+                claims.Add(new Claim(JwtClaimTypes.Role, role));
 
-                    //se roleManager suportar claims para roles
-                    if (_roleManager.SupportsRoleClaims)
+                //se roleManager suportar claims para roles
+                if (_roleManager.SupportsRoleClaims)
+                {
+                    //localiza o perfil
+                    IdentityRole identityRole = await _roleManager
+                                                      .FindByNameAsync(role);
+                    //inclui o perfil
+                    if (identityRole != null)
                     {
-                        //localiza o perfil
-                        IdentityRole identityRole = await _roleManager
-                                                          .FindByNameAsync(role);
-                        //inclui o perfil
-                        if (identityRole != null)
-                        {
-                            //inclui as claims associada com a role
-                            claims.AddRange(await _roleManager
-                                .GetClaimsAsync(identityRole));
-                        }
+                        //inclui as claims associada com a role
+                        claims.AddRange(await _roleManager
+                            .GetClaimsAsync(identityRole));
                     }
                 }
             }
-            //retorna as claims no contexto
-            context.IssuedClaims = claims;
+        }
+        //retorna as claims no contexto
+        context.IssuedClaims = claims;
     }
 
     public async Task IsActiveAsync(IsActiveContext context)
