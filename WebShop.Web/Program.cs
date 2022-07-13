499c14e075f6be932df43568be1e1123064be56c
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-07-13 11:16:42 -0300

    Configure IdentityServer Authentication

--- WebShop.Web/Program.cs
+++ WebShop.Web/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication;
 using WebShop.Web.Services;
 using WebShop.Web.Services.Contracts;
 
@@ -6,6 +7,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+    })
+    .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = builder.Configuration["ServiceUri:IdentityServer"];
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.ClientId = "webshop";
+        options.ClientSecret = builder.Configuration["Client:Secret"];
+        options.ResponseType = "code";
+        options.ClaimActions.MapJsonKey("role", "role", "role");
+        options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+        options.Scope.Add("webshop");
+        options.SaveTokens = true;
+    });
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient("ProductApi", c =>
@@ -28,6 +50,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // Always come before authorization
 app.UseAuthorization();
 
 app.MapControllerRoute(
