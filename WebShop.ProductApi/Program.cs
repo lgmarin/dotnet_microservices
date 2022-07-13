253893f7c2c5d15ae6d0be6d4549f1a907d58cba
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-07-13 11:29:34 -0300

    Configure API

--- WebShop.ProductApi/Program.cs
+++ WebShop.ProductApi/Program.cs
@@ -1,6 +1,7 @@
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using WebShop.ProductApi.Context;
 using WebShop.ProductApi.Repositories;
 using WebShop.ProductApi.Services;
@@ -13,7 +14,36 @@ builder.Services.AddControllers().AddJsonOptions(x =>
     x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "VShop.ProductApi", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"'Bearer' [space] seu token",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement 
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In= ParameterLocation.Header
+            },
+            new List<string> ()
+        }
+    });
+});
 
 // DATABASE CONNECTION
 var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
