918219edcedf68ac1cb2b92eefd2fef53b26f87b
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-10 15:36:14 -0300

    Create CartController Routes

--- WebShop.CartApi/Controllers/CartController.cs
+++ WebShop.CartApi/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WebShop.CartApi.DTOs;
 using WebShop.CartApi.Repositories;
 
 namespace WebShop.CartApi.Controllers;
@@ -13,5 +14,29 @@ public class CartController : ControllerBase
     public CartController(ICartRepository repository)
     {
         _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<CartDTO>> GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CartDTO>> AddCart(CartDTO cartDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<CartDTO>> UpdateCart(CartDTO cartDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<bool>> DeleteCart(int id)
+    {
+        throw new NotImplementedException();
     }
 }
