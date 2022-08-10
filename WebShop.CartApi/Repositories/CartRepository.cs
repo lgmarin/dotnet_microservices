16660990e3986f89f7dd29873597c5d2b028b8d1
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-10 15:55:30 -0300

    Fix some methods name and create methods

--- WebShop.CartApi/Repositories/CartRepository.cs
+++ WebShop.CartApi/Repositories/CartRepository.cs
@@ -1,36 +1,58 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WebShop.CartApi.Context;
 using WebShop.CartApi.DTOs;
+using WebShop.CartApi.Models;
 
 namespace WebShop.CartApi.Repositories;
 
 public class Cartrepository : ICartRepository
 {
-    public async Task<CartDTO> GetCartByUserIdAsync(string userId)
+    private readonly AppDbContext _context;
+    private IMapper _mapper;
+
+    public Cartrepository(AppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<CartDTO> GetCartByUserId(string userId)
+    {
+        Cart cart = new Cart()
+        {
+            CartHeader = await _context.CartHeaders.FirstOrDefaultAsync(c => c.UserId == userId)
+        };
+
+        cart.CartItems = _context.CartItems.Where(c => c.CartHeaderId == cart.CartHeader.Id)
+            .Include(c => c.Product);
+
+        return _mapper.Map<CartDTO>(cart);
+    }
+
+    public async Task<CartDTO> UpdateCart(CartDTO cart)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<CartDTO> UpdateCartAsync(CartDTO cart)
+    public async Task<bool> DeleteItemCart(string userId, string couponCode)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public async Task<bool> CleanCart(string userId)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<bool> DeleteItemCartAsync(string userId, string couponCode)
+    public async Task<bool> ApplyCoupon(string userId)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<bool> ApplyCouponAsync(string userId)
+    public async Task<bool> DeleteCoupon(string userId)
     {
         throw new NotImplementedException();
     }
-
-    public async Task<bool> DeleteCouponAsync(string userId)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task<bool> CleanCartAsync(string userId)
-    {
-        throw new NotImplementedException();
-    }
+    
 }
--- WebShop.CartApi/Repositories/ICartRepository.cs
+++ WebShop.CartApi/Repositories/ICartRepository.cs
@@ -4,10 +4,10 @@ namespace WebShop.CartApi.Repositories;
 
 public interface ICartRepository
 {
-    Task<CartDTO> GetCartByUserIdAsync(string userId);
-    Task<CartDTO> UpdateCartAsync(CartDTO cart);
-    Task<bool> DeleteItemCartAsync(string userId, string couponCode);
-    Task<bool> ApplyCouponAsync(string userId);
-    Task<bool> DeleteCouponAsync(string userId);
-    Task<bool> CleanCartAsync(string userId);
+    Task<CartDTO> GetCartByUserId(string userId);
+    Task<CartDTO> UpdateCart(CartDTO cart);
+    Task<bool> DeleteItemCart(string userId, string couponCode);
+    Task<bool> ApplyCoupon(string userId);
+    Task<bool> DeleteCoupon(string userId);
+    Task<bool> CleanCart(string userId);
 }
