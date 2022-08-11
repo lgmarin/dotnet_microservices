644fc6d53e3e396d1613ae49297516eb406ec820
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-11 08:51:21 -0300

    Create CartHeader VM

--- WebShop.Web/Models/CartItemViewModel.cs
+++ WebShop.Web/Models/CartItemViewModel.cs
@@ -3,7 +3,7 @@ namespace WebShop.Web.Models;
 public class CartItemViewModel
 {
     public int Id { get; set; }
-    public int Quantity { get; set; }
+    public int Quantity { get; set; } = 1;
     public int ProductId { get; set; }
     public int CartHeaderId { get; set; }
     public ProductViewModel Product { get; set; } = new ProductViewModel();
--- WebShop.Web/Models/CartViewModel.cs
+++ WebShop.Web/Models/CartViewModel.cs
@@ -2,6 +2,6 @@ namespace WebShop.Web.Models;
 
 public class CartViewModel
 {
-    public CarHeaderViewModel CartHeader { get; set; } = new CarHeaderViewModel();
+    public CartHeaderViewModel CartHeader { get; set; } = new CartHeaderViewModel();
     public IEnumerable<CartItemViewModel> CartItems { get; set; } = Enumerable.Empty<CartItemViewModel>();
 }
