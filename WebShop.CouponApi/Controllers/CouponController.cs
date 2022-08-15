7bbec7d9e4fa4812b48c1b1871d0c1ba5800dd22
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-15 10:22:06 -0300

    Create Controller

---
+++ WebShop.CouponApi/Controllers/CouponController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using WebShop.CouponApi.DTOs;
+using WebShop.CouponApi.Repositories;
+
+namespace WebShop.CouponApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CouponController : ControllerBase
+{
+    private ICouponRepository _repository;
+
+    public CouponController(ICouponRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<ActionResult<CouponDTO>> GetDiscountByCode(string couponCode)
+    {
+        var coupon = await _repository.GetCouponByCode(couponCode);
+
+        if (coupon is null)
+        {
+            return NotFound($"Coupon code: {couponCode} not found!");
+        }
+
+        return Ok(coupon);
+    }
+}
