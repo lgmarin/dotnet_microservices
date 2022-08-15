1187e4222ed61fc4f7242fea226eb8ab3b1e438c
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-15 09:52:13 -0300

    Create Coupon repository

--- WebShop.CouponApi/Context/AppDbContext.cs
+++ WebShop.CouponApi/Context/AppDbContext.cs
@@ -8,7 +8,7 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    private DbSet<Coupon>? Discounts { get; set; }
+    public DbSet<Coupon>? Coupons { get; set; }
 
     protected override void OnModelCreating(ModelBuilder mb)
     {
