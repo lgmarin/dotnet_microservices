7b0ed0da5fd5ab7394491927cd95b64b77a52e94
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-16 08:42:17 -0300

    Delete Coupon

--- WebShop.CartApi/Repositories/CartRepository.cs
+++ WebShop.CartApi/Repositories/CartRepository.cs
@@ -99,16 +99,7 @@ public class Cartrepository : ICartRepository
         return false;
     }
 
-    public async Task<bool> ApplyCoupon(string userId, string couponCode)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task<bool> DeleteCoupon(string userId, string couponCode)
-    {
-        throw new NotImplementedException();
-    }
-    
+  
     private async Task SaveProductInDB(CartDTO cartDto, Cart cart)
     {
         var product = await _context.Products.FirstOrDefaultAsync(p =>
@@ -155,5 +146,29 @@ public class Cartrepository : ICartRepository
             cart.CartItems.FirstOrDefault().CartHeaderId = cartDetail.CartHeaderId;
             _context.CartItems.Update(cart.CartItems.FirstOrDefault());
         }
+    }
+    
+    public async Task<bool> ApplyCoupon(string userId, string couponCode)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteCoupon(string userId, string couponCode)
+    {
+        var cartHeaderDel = await _context.CartHeaders.
+            FirstOrDefaultAsync(c => c.UserId == userId);
+
+        if (cartHeaderDel is not null)
+        {
+            cartHeaderDel.CouponCode = "";
+
+            _context.CartHeaders.Update(cartHeaderDel);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        return false;
     }
 }
