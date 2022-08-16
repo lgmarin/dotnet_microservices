0469603bff929c5a8155381a6be0cc687a03fce9
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-16 13:48:17 -0300

    Create Coupon VM

--- WebShop.Web/Models/CouponViewModel.cs
+++ WebShop.Web/Models/CouponViewModel.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace WebShop.Web.Models;
 
 public class CouponViewModel
 {
+    public int CouponId { get; set; }
+
+    [Required]
+    [StringLength(30)]
+    public string? CouponCode { get; set; }
     
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
+    public decimal Discount { get; set; }
 }
