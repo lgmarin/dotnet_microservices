785a6347449ecd4b92a72b117611860459ee20e2
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-16 08:54:36 -0300

    Apply Coupon controller

--- WebShop.CartApi/Controllers/CartController.cs
+++ WebShop.CartApi/Controllers/CartController.cs
@@ -59,4 +59,17 @@ public class CartController : ControllerBase
 
         return Ok(status);
     }
+
+    [HttpPost("applycoupon")]
+    public async Task<ActionResult<CartDTO>> ApplyCoupon(CartDTO cartDto)
+    {
+        var result = await _repository.ApplyCoupon(cartDto.CartHeader.UserId, cartDto.CartHeader.CouponCode);
+
+        if (!result)
+        {
+            return NotFound($"CartHeader not found for userId: {cartDto.CartHeader.UserId}");
+        }
+
+        return Ok(result);
+    }
 }
