6b11fe398caea13fdb822f242fd40f4ecf4a8a33
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-22 08:40:01 -0300

    Adjust CartHeader VM

--- WebShop.Web/Models/CartHeaderViewModel.cs
+++ WebShop.Web/Models/CartHeaderViewModel.cs
@@ -7,4 +7,11 @@ public class CartHeaderViewModel
     public string CouponCode { get; set; } = string.Empty;
     public decimal TotalAmount { get; set; } = 0.00m;
     public decimal Discount { get; set; } = 0.00m;
+    
+    // Checkout data
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateTime { get; set; }
+    public string Telephone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
