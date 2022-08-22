4a769e670f0fb32d654eee1fabaf3472e8fd19d6
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-22 11:18:14 -0300

    Create Checkout DTO

---
+++ WebShop.CartApi/DTOs/CheckoutHeaderDTO.cs
@@ -0,0 +1,21 @@
+namespace WebShop.CartApi.DTOs;
+
+public class CheckoutHeaderDTO
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string CouponCode { get; set; } = string.Empty;
+    public decimal TotalAmount { get; set; } = 0.00m;
+    public decimal Discount { get; set; } = 0.00m;
+    
+    // Checkout data
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateTime { get; set; }
+    public string Telephone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string CardNumber { get; set; } = string.Empty;
+    public string NameOnCard { get; set; } = string.Empty;
+    public string CVV { get; set; } = string.Empty;
+    public string ExpireMonthYear { get; set; } = string.Empty; 
+}
